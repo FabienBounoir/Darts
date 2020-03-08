39006bc3fbe402295a7c60e16035ee329681abf6
tvaira <tvaira@a2959681-d039-4abf-ac63-aa19f0c864f2>
2020-03-08 09:18:52 +0000

    Revision de code

--- trunk/mobile-DARTS/app/src/main/java/projet/lasalle84/darts/TypeJeu.java
+++ trunk/mobile-DARTS/app/src/main/java/projet/lasalle84/darts/TypeJeu.java
@@ -1,32 +1,84 @@
 package projet.lasalle84.darts;
 
+/**
+ * @file TypeJeu.java
+ * @brief Déclaration de la classe TypeJeu
+ * @author Menella Erwan
+ */
 
-public class TypeJeu {
+/**
+ * @class TypeJeu
+ * @brief Déclaration de la classe TypeJeu
+ */
+public class TypeJeu
+{
+    /**
+     * Attributs
+     */
+    private boolean doubleOut;  //!<
+    private int pointDepart;    //!<
 
-    private boolean doubleOut;
-    private int pointDepart;
+    /**
+     * Constantes
+     */
+    private final static int PARTIE_501 = 0;
+    private final static int PARTIE_501_DOUBLE_OUT = 1;
+    private final static int PARTIE_301 = 2;
+    private final static int PARTIE_301_DOUBLE_OUT = 3;
 
+    /**
+     * @brief Constructeur par défaut de la classe TypeJeu
+     *
+     * @fn TypeJeu::TypeJeu()
+     *
+     */
     public TypeJeu()
     {
-
+        this.pointDepart = 501;
+        this.doubleOut = true;
     }
 
+    /**
+     * @brief Constructeur de la classe TypeJeu
+     *
+     * @fn TypeJeu::TypeJeu(int pointDepart, boolean doubleOut)
+     * @param pointDepart
+     * @param doubleOut
+     */
     public TypeJeu(int pointDepart, boolean doubleOut)
     {
         this.pointDepart = pointDepart;
         this.doubleOut = doubleOut;
     }
 
+    /**
+     * @brief Constructeur de la classe TypeJeu
+     *
+     * @fn TypeJeu::TypeJeu(int idModeJeu)
+     * @param idModeJeu l'identifiant d'un type de jeu
+     */
     public TypeJeu(int idModeJeu)
     {
         switch (idModeJeu)
         {
-            case 0:
+            case PARTIE_501:
+                this.pointDepart = 501;
+                this.doubleOut = false;
+                break;
+            case PARTIE_501_DOUBLE_OUT:
                 this.pointDepart = 501;
                 this.doubleOut = true;
                 break;
-            case 1:
+            case PARTIE_301:
                 this.pointDepart = 301;
+                this.doubleOut = false;
+                break;
+            case PARTIE_301_DOUBLE_OUT:
+                this.pointDepart = 301;
+                this.doubleOut = true;
+                break;
+            default:
+                this.pointDepart = 501;
                 this.doubleOut = true;
                 break;
         }
@@ -50,6 +102,4 @@ public class TypeJeu {
     {
         this.doubleOut = doubleOut;
     }
-
-
 }
